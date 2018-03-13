eea811365509477a6127cf62e401bcf7768733e8
josdem <joseluis.delacruz@gmail.com>
2018-03-12 19:48:14 -0600

    Fixing packages names

--- src/main/groovy/com/jos/dem/springboot/validator/DemoApplication.groovy
+++ src/main/groovy/com/jos/dem/springboot/validator/DemoApplication.groovy
@@ -1,4 +1,4 @@
-package com.example.springbootvalidation
+package com.jos.dem.springboot.validator
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -9,4 +9,5 @@ class DemoApplication {
 	static void main(String[] args) {
 		SpringApplication.run DemoApplication, args
 	}
+
 }
