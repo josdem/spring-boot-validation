70fed599134895cb1c16a249c74fbb191f054c8d
josdem <joseluis.delacruz@gmail.com>
2024-08-15 07:19:21 -0400

    Adding test

--- src/test/java/com/josdem/springboot/validation/ValidatorApplicationTests.java
+++ src/test/java/com/josdem/springboot/validation/ValidatorApplicationTests.java
@@ -1,12 +1,31 @@
 package com.josdem.springboot.validation;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@Slf4j
 @SpringBootTest
+@AutoConfigureMockMvc
 class ValidatorApplicationTests {
 
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("showing index page")
+    void shouldReturnIndex(TestInfo testInfo) throws Exception {
+        log.info("Running: {}", testInfo.getDisplayName());
+        mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"));
+    }
 
 }
